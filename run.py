0dca7ccd31e3de4aa6e94935f1c4742d55e445b6
Justin Whittaker <just.whittaker@gmail.com>
2020-12-19 17:10:06 +0000

    fix(RecipeCloud): fix the index search option to search multiple items on the db

--- run.py
+++ run.py
@@ -28,7 +28,14 @@ def index():
 @app.route("/get_recipes")
 def get_recipes():
     recipes = mongo.db.recipes.find()
-    return render_template(get_recipes.html, recipes=recipes)
+    return render_template("index.html", recipes=recipes)
+
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    recipes = list(mongo.db.recipes.find({"$text": {"$search": query}}))
+    return render_template("index.html", recipes=recipes)
 
 
 @app.route("/register", methods=["GET", "POST"])
