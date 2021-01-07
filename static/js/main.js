01af6cc3cf7f0623b981bce3111b0a5b738eff10
Justin Whittaker <just.whittaker@gmail.com>
2021-01-07 10:01:17 +0000

    style(RecipeCloud): change hero img, adjust styling in CSS

--- static/js/main.js
+++ static/js/main.js
@@ -30,5 +30,10 @@ function mainMenu() {
 $('.dropdown-toggle').dropdown();
 }
 
+$(document).ready(function() {
+  $("#myButton").click(function() {
+    $("#myButton").after('<input type="text" id="textInput" value="">');
+  });
+});
 
 $(document).ready(mainMenu);
